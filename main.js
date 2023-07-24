963b5643fc2f04cc0a799d25a098913efbed0df3
Iqbal <89727636+Iqbaleditzz@users.noreply.github.com>
2023-07-24 14:32:46 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+const popupOverlay = document.querySelector(".popup-overlay");
+
+const popupClose = document.querySelector(".popup-close");
+
+
+
+popupClose.addEventListener("click", () => {
+
+  popupOverlay.style.display = "none";
+
+  localStorage.setItem("popupDisplayed", "true");
+
+});
+
+
+
+setTimeout(() => {
+
+  if (!localStorage.getItem("popupDisplayed")) {
+
+    popupOverlay.style.display = "block";
+
+  }
+
+}, 3000);
